78e0093ff2b2af61921445c858c253b509243915
Hanan <Hanan.bayazeed56@gmail.com>
2026-02-09 19:34:13 +0300

    create axios instances+config-index

---
+++ client/src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import config from "../config/index";
+const { apiUrl } = config;
+
+export default axios.create({ baseURL: apiUrl }); //this is used for public routes
+
+export const axiosPrivate = axios.create({
+  baseURL: apiUrl,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+}); //this is used for private routes
---
+++ client/src/config/index.js
@@ -0,0 +1,8 @@
+const config = {
+  apiUrl: process.env.REACT_APP_API_URL,
+  enableLogs: process.env.REACT_APP_ENABLE_LOGS === "true",
+  isProd: process.env.NODE_ENV === "production",
+  isDev: process.env.NODE_ENV === "development",
+};
+
+export default config;
